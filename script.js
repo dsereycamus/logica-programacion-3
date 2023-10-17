483d92266d2f8b2d350127280f6b158db8dc2ccc
Daniela Serey <dsereycamus@gmail.com>
2023-10-17 16:02:39 -0300

    Inicializando práctica de factorial

---
+++ script.js
@@ -0,0 +1,28 @@
+function calcularFactorial() {
+    var numero = document.getElementById('numeroIngresado').value;
+
+    // Verificar si el valor ingresado es un número entero
+    if (!/^\d+$/.test(numero)) {
+        alert('Por favor, ingrese un número entero válido.');
+        return;
+    }
+
+    numero = parseInt(numero); // Convierte a número entero si es un número válido
+    var resultado = 1;
+    for (var i = 1; i <= numero; i++) {
+        resultado = resultado * i;
+    }
+    document.getElementById('textoResultado').innerHTML = `El factorial del número ${numero} es: ${resultado}`;
+}
+
+// function calcularFactorial(){
+
+// var numero=document.getElementById('numeroIngresado').value;
+// var resultado=1;
+// for (var i=1; i<=numero; i++){
+//     resultado=resultado*i;
+// }
+// document.getElementById('textoResultado').innerHTML= `El factorial del número $(numero) es: $(resultado)`;
+
+// }
+
