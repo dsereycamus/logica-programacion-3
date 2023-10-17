cb227ea983b19f5eddc3a9105c4aec57fc28b047
Daniela Serey <dsereycamus@gmail.com>
2023-10-17 16:26:50 -0300

    Integrando prompt y color a factorial

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function calcularFactorial() {
-    var numero = document.getElementById('numeroIngresado').value;
+function solicitarNumeroYCalcularFactorial() {
+    var numero = prompt('Ingrese un número entero:');
+    
+    if (numero === null || numero === "") {
+        alert('Por favor, ingrese un número.');
+        return;
+    }
 
     // Verificar si el valor ingresado es un número entero
     if (!/^\d+$/.test(numero)) {
@@ -8,13 +13,16 @@ function calcularFactorial() {
     }
 
     numero = parseInt(numero); // Convierte a número entero si es un número válido
+    calcularFactorial(numero);
+}
+
+function calcularFactorial(numero) {
     var resultado = 1;
     for (var i = 1; i <= numero; i++) {
         resultado = resultado * i;
     }
     document.getElementById('textoResultado').innerHTML = `El factorial del número ${numero} es: ${resultado}`;
 }
-
 // function calcularFactorial(){
 
 // var numero=document.getElementById('numeroIngresado').value;
